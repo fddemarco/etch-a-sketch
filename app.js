7c8e59f9cf9a58104d691658b11be5ab0a73750e
fddemarco <franco.demarco3400@gmail.com>
2023-07-20 23:20:51 -0300

    feat: hovering over canvas leaves behind trail

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+function fillPixel(event) {
+  const pixel = event.target;
+  pixel.style.background = 'black';
+}
+
 const canvas = document.querySelector('.canvas');
 
 for (let i = 0; i < 16*16; i++){
   const canvasPixel = document.createElement('div');
   canvasPixel.classList.add('canvas-pixel');
   canvas.appendChild(canvasPixel);
+  canvasPixel.addEventListener('mouseover', fillPixel)
 }
