052b5a6dbf6107b9f46109a3cc9e9c3d5f15b459
fddemarco <franco.demarco3400@gmail.com>
2023-07-21 00:03:51 -0300

    feat: change canvas resolution button

--- app.js
+++ app.js
@@ -3,11 +3,37 @@ function fillPixel(event) {
   pixel.style.background = 'black';
 }
 
-const canvas = document.querySelector('.canvas');
-
-for (let i = 0; i < 16*16; i++){
-  const canvasPixel = document.createElement('div');
-  canvasPixel.classList.add('canvas-pixel');
-  canvas.appendChild(canvasPixel);
-  canvasPixel.addEventListener('mouseover', fillPixel)
+function changeCanvasResolution(event){
+  const resolution = +prompt('Please input canvas width resolution');
+  const canvas = document.querySelector('.canvas');
+  cleanCanvas(canvas);
+  drawCanvas(canvas, resolution);
 }
+
+function cleanCanvas(canvas){
+  console.log(canvas);
+  const pixels = canvas.querySelectorAll('.canvas-pixel');
+  for (let pixel of pixels){
+    canvas.removeChild(pixel);
+  }
+}
+
+function drawCanvas(canvas, resolution){
+  const pixelLength = `${16*16/resolution}px`;
+  for (let i = 0; i < resolution*resolution; i++){
+    const canvasPixel = document.createElement('div');
+    canvasPixel.classList.add('canvas-pixel');
+
+    canvasPixel.style.width = pixelLength;
+    canvasPixel.style.height = pixelLength;
+
+    canvas.appendChild(canvasPixel);
+    canvasPixel.addEventListener('mouseover', fillPixel)
+  }
+}
+
+const canvas = document.querySelector('.canvas');
+drawCanvas(canvas, 16);
+
+const canvasResolutionButton = document.querySelector('.canvas-resolution-button');
+canvasResolutionButton.addEventListener('click', changeCanvasResolution);
